6229fb9d46316ff0cb4c911d6cc66565e9eae7c3
Sano Hirofumi <132859349+sanhir@users.noreply.github.com>
2024-06-24 03:48:46 +0000

    deploy

--- .github/scripts/convertMarkdownToMrkdwn.js
+++ .github/scripts/convertMarkdownToMrkdwn.js
@@ -4,11 +4,11 @@
 const marked = require("marked");
 
 function markdownToHtml(markdown) {
-    return marked.parse(markdown);
+  return marked.parse(markdown);
 }
 
 function htmlToMrkdwn(html) {
-    return html
+  return html
     .replace(/<a href=\\"(.*?)\\">(.*?)<\/a>/g, "<$1|$2>")
     .replace(/<strong>(.*?)<\/strong>/g, "*$1*")
     .replace(/<em>(.*?)<\/em>/g, "_$1_")
@@ -17,20 +17,29 @@ function htmlToMrkdwn(html) {
     .replace(/<h1>(.*?)<\/h1>/g, "*$1*\n")
     .replace(/<h2>(.*?)<\/h2>/g, "*$1*\n")
     .replace(/<h3>(.*?)<\/h3>/g, "*$1*\n")
-    .replace(/<ul>(.*?)<\/ul>/gs, (match, p1) => p1.replace(/<li>(.*?)<\/li>/g, "• $1\n"))
-    .replace(/<ol>(.*?)<\/ol>/gs, (match, p1) => p1.replace(/<li>(.*?)<\/li>/g, "• $1\n"))
-    .replace(/<blockquote>(.*?)<\/blockquote>/gs, (match, p1) => p1.split("\n").map(line => line && `> ${line}`).join("\n"))
+    .replace(/<ul>(.*?)<\/ul>/gs, (match, p1) =>
+      p1.replace(/<li>(.*?)<\/li>/g, "• $1\n"),
+    )
+    .replace(/<ol>(.*?)<\/ol>/gs, (match, p1) =>
+      p1.replace(/<li>(.*?)<\/li>/g, "• $1\n"),
+    )
+    .replace(/<blockquote>(.*?)<\/blockquote>/gs, (match, p1) =>
+      p1
+        .split("\n")
+        .map((line) => line && `> ${line}`)
+        .join("\n"),
+    )
     .replace(/<p>(.*?)<\/p>/g, "$1\n")
     .replace(/<\/?[^>]+(>|$)/g, "")
     .replace(/\\/g, "\\\\")
-    .replace(/\`/g, "\\\`")
-    .replace(/\"/g, "\\\"")
+    .replace(/\`/g, "\\`")
+    .replace(/\"/g, '\\"')
     .replace(/\$/g, "\\$");
 }
 
 function markdownToMrkdwn(markdown) {
-    const html = markdownToHtml(markdown);
-    return htmlToMrkdwn(html);
+  const html = markdownToHtml(markdown);
+  return htmlToMrkdwn(html);
 }
 
 // 与えられた文字列をmrkdwnに変換
