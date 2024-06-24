4457b84ef971d6cdef37d115bf3d23ed99af7566
Sano Hirofumi <132859349+sanhir@users.noreply.github.com>
2024-06-24 03:56:48 +0000

    a

--- .github/scripts/convertMentions.js
+++ .github/scripts/convertMentions.js
@@ -1,4 +1,22 @@
-// 文字列のバッククォートをエスケープするスクリプト
-const text = process.argv[2];
-const result = String(text.replace(/\`/g, "\\`"));
-console.log(`sanitizedTitle<<EOF\n${result}\nEOF`);
+/**
+ * GithubのメンションをSlackメンションに変換するスクリプト
+ **/
+const fs = require("fs");
+
+// ユーザーマッピングを読み込む
+const userMapping = JSON.parse(
+  fs.readFileSync(".github/scripts/github-slack_user_mapping.json", "utf8"),
+);
+
+// メッセージを定義するgithub-slack_user_mapping
+let message = process.argv[2];
+
+// GitHubユーザー名をSlackユーザーIDに変換する
+for (const [githubUser, slackUser] of Object.entries(userMapping)) {
+  const githubMention = `@${githubUser}`;
+  const slackMention = `<@${slackUser}>`;
+  message = message.replace(new RegExp(githubMention, "g"), slackMention);
+}
+
+// 変換後のメッセージを環境変数に設定する
+console.log(`commentBody<<EOF\n${message}\nEOF`);
--- .github/scripts/sanitizeTitleBackquote.js
+++ .github/scripts/sanitizeTitleBackquote.js
@@ -1,4 +1,4 @@
 // 文字列のバッククォートをエスケープするスクリプト
 const text = process.argv[2];
-const result = String(text.replace(/\`/g, "\\\`"));
+const result = String(text.replace(/\`/g, "\\`"));
 console.log(`sanitizedTitle<<EOF\n${result}\nEOF`);
